56b94a0e8b0aaa3b74db6d3cf69f7573df963254
hanaiping <hanaiping@qq.com>
2018-10-14 17:26:51 +0800

    添加工程到git中

---
+++ gittest/gitabc/src/com/itheima/FirstClass.java
@@ -0,0 +1,12 @@
+package com.itheima;
+
+public class FirstClass {
+
+    public static void main(String[] args) {
+        System.out.println("firstclass");
+        System.out.println("firstclass");
+        System.out.println("firstclass");
+        System.out.println("firstclass");
+        System.out.println("firstclass");
+    }
+}
